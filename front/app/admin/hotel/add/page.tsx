c1e417cdef3e2ac15bbd63a1ca0004d37efad5da
bloodjens1023 <bloodjens32@gmail.com>
2024-07-02 10:06:51 +0300

    fdfd

--- front/app/admin/hotel/add/page.tsx
+++ front/app/admin/hotel/add/page.tsx
@@ -8,11 +8,11 @@ import FormHotel from "@/components/FormHotel";
 export default function Component() {
   let value = "bg-accent";
   return (
-    <div className="flex min-h-screen w-full flex-col bg-muted/40">
+    <div className="flex flex-col w-full min-h-screen bg-muted/40">
       <Navadmin dash={""} hotels={value} craft={""} tour={""} client={""} />
       <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
         <HeaderAdmin name="Hotel" more="more" />
-        <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 mt-5">
+        <main className="grid items-start flex-1 gap-4 p-4 mt-5 sm:px-6 sm:py-0 md:gap-8">
           <FormHotel />
         </main>
       </div>
--- front/app/admin/hotel/show/page.tsx
+++ front/app/admin/hotel/show/page.tsx
@@ -7,11 +7,11 @@ import ListHotel from "@/components/ListHotel";
 export default function Component() {
   let value = "bg-accent";
   return (
-    <div className="flex min-h-screen w-full flex-col bg-muted/40">
+    <div className="flex flex-col w-full min-h-screen bg-muted/40">
       <Navadmin dash={""} hotels={value} craft={""} tour={""} client={""} />
       <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
         <HeaderAdmin name="Hotel" more="List" />
-        <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 mt-5">
+        <main className="grid items-start flex-1 gap-4 p-4 mt-5 sm:px-6 sm:py-0 md:gap-8">
           <ListHotel />
         </main>
       </div>
