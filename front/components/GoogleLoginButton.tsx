207eb7a2b0a471ef9224030d360b03da1d6135e4
Brad Akemi <mirado.akemi@gmail.com>
2024-07-11 17:38:06 +0300

    firebase

--- front/components/GoogleLoginButton.tsx
+++ front/components/GoogleLoginButton.tsx
@@ -1,31 +1,62 @@
 // components/GoogleLoginButton.tsx
-import { signIn } from "next-auth/react";
+import { initializeApp } from "firebase/app";
+import { GoogleAuthProvider, getAuth, signInWithPopup, signInWithRedirect } from "firebase/auth";
 
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: process.env.GOOGLE_API_FIREBASE,
+    authDomain: process.env.GOOGLE_FIREBASE_AUTHDOMAIN,
+    projectId: "test-ce224",
+    storageBucket: "test-ce224.appspot.com",
+    messagingSenderId: "758626351874",
+    appId: "1:758626351874:web:4e0e954eb74dde33c3a01b",
+    measurementId: "G-9B4HNZLJ79"
+};
+
+const app = initializeApp(firebaseConfig);
 const GoogleLoginButton = () => {
     const handleSignIn = () => {
-        signIn("google");
+        const provider = new GoogleAuthProvider();
+        provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+        const auth = getAuth(app);
+        auth.languageCode = 'fr';
+        provider.setCustomParameters({
+            'login_hint': 'user@example.com'
+        });
+
+        signInWithRedirect(auth, provider)
+            .then((result) => {
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+
+                if (credential) {
+                    const token = credential.accessToken;
+                }
+
+                // The signed-in user info.
+                const user = result.user;
+
+                console.log(user);
+
+            }).catch((error) => {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // The email of the user's account used.
+                const email = error.customData.email;
+                // The AuthCredential type that was used.
+                const credential = GoogleAuthProvider.credentialFromError(error);
+            });
     };
 
     return (
-
         <button onClick={handleSignIn}>
-            <SessionProvider>
-
-                {/* <Button */}
-                variant="outline"
-                type="button"
-                className="w-full mb-4 flex items-center justify-center gap-5 rounded-none"
-                >
-
-                <Image src={chrome} width={20} height={20} alt="chrome" />
-                <GoogleLoginButton />
-                {/* <span>Sign up with Google</span> */}
-
-                {/* <GoogleSignupButton /> */}
-
-            </SessionProvider>
-        </Button>
-
+            Log in with Google
+        </button>
     );
 };
 
