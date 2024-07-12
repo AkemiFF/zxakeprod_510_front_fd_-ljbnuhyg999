e2711880d6038e23359e3e29d89974cb2f4743d9
Daniel <Razdaniel25@gmail.com>
2024-07-12 10:30:50 +0300

    Integration ListCrafts

--- front/lib/config.ts
+++ front/lib/config.ts
@@ -1,9 +1,5 @@
 const Urlconfig = {
-<<<<<<< HEAD
-    apiBaseUrl: 'http://127.0.0.1:8000/',
-=======
-    apiBaseUrl: 'http://192.168.88.20:8000/',
->>>>>>> origin/Brad
+    apiBaseUrl: 'http://127.0.0.1:8000',
     adminUrl: '/admin',
 };
 
